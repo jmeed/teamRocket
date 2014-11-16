5cf591d3c7aab6de8e0c9b8da368c491b6db093a
Arthur <arajala@umich.edu>
2014-11-16 16:15:38 -0500

    no clue what this changed

--- src/xbee/xbee_setup.m
+++ src/xbee/xbee_setup.m
@@ -1,10 +1,19 @@
-function x_port = xbee_setup(com_port, data_bits, stop_bits, baud_rate, parity)
+function xbee_setup(com_port, baud_rate)
+% Returns a serial port handle that the XBee is configured on, and sets up
+% callback parsing functions to read data from the rockets accelerometer,
+% barometer, and GPS module
 
+% Initialize serial port
 x_port = serial(com_port);
-set(x_port, 'DataBits', data_bits);
-set(x_port, 'StopBits', stop_bits);
+
+% Configure serial port
 set(x_port, 'BaudRate', baud_rate);
-set(x_port, 'Parity', parity);
+set(x_port, 'DataBits', 8);
+set(x_port, 'StopBits', 1);
+set(x_port, 'Parity', 'none');
+set(x_port, 'ByteOrder', 'littleEndian');
+
+% Open serial port
 fopen(x_port);
 
 % Send acknowledgement
@@ -18,5 +27,17 @@ while (a ~= 'a')
 end
 disp('Received acknowledgement')
 disp('XBee communication ready')
+
+% Setup callbacks
+x_port.BytesAvailableFcnCount = 4*6 + 1*1;  % 6 floats, 1 char
+                                            % altitude
+                                            % temp
+                                            % x-axis acceleration
+                                            % GPS x-coordinate
+                                            % GPS y-coordinate
+                                            % GPS z-coordinate
+                                            % start character 'S'
+x_port.BytesAvailableFcnMode = 'byte';
+x_port.BytesAvailableFcn = @xbee_station;
 
 end
