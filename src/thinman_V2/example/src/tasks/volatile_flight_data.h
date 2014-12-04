29a20690dc31c7e151e978a9d94c2fa2f8d87dd3
Patrick D'Agostino <patdag@umich.edu>
2014-12-04 02:29:13 -0500

    added in status functionality, not tested

--- src/thinman_V2/example/src/tasks/volatile_flight_data.h
+++ src/thinman_V2/example/src/tasks/volatile_flight_data.h
@@ -13,4 +13,10 @@ float descent_rate;
 float alt_arr[5];
 float time_arr[5];
 
+extern static bool volt_active;
+extern static bool gps_activated;
+extern static bool baro_running;
+extern static bool imu_running;
+extern static bool highg_running;
+
 #endif /* VOLATILE_FLIGHT_DATA_H */
